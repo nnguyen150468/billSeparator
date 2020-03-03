6a4a267e897f4e116d17cf67ada014a668943427
Nguyen Nguyen <n.dinhnguyen95@gmail.com>
2020-03-03 23:11:32 +0700

    validate the input to make sure it's positive

--- main.js
+++ main.js
@@ -7,36 +7,40 @@ const k = 1000;
 
 function getBill(){
     bill = document.getElementById("bill").value;
+    if(bill<0){
+        document.getElementById("resultArea").innerHTML="Bill must be a positive number.";
+        getBill();
+    }
 }
 
 function calculate(){
-    if(bill>=(500*k)){
-        num500k=Math.floor((bill/(500*k)));
-        remainder=bill - num500k*500*k;
-    } if(remainder>=(200*k)){
-        num200k=Math.floor((remainder/(200*k)));
-        remainder=remainder - num200k*200*k;
-    } if (remainder>=(100*k)){
-        num100k=Math.floor((remainder/(100*k)));
-        remainder=remainder - num100k*100*k;
-    } if (remainder>=(50*k)){
-        num50k=Math.floor((remainder/(50*k)));
-        remainder=remainder - num50k*50*k;
-    } if (remainder>=(20*k)){
-        num20k=Math.floor((remainder/(20*k)));
-        remainder=remainder - num20k*20*k;
-    } if (remainder>=(10*k)){
-        num10k=Math.floor((remainder/(10*k)));
-        remainder=remainder - num10k*10*k;
-    } if (remainder>=(5*k)){
-        num5k=Math.floor((remainder/(5*k)));
-        remainder=remainder - num5k*5*k;
-    } if (remainder>=(2*k)){
-        num2k=Math.floor((remainder/(2*k)));
-        remainder=remainder - num2k*2*k;
-    } if (remainder>=(1*k)){
-        num1k=Math.floor((remainder/(1*k)));
-    }    
+        if(bill>=(500*k)){
+            num500k=Math.floor((bill/(500*k)));
+            remainder=bill - num500k*500*k;
+        } if(remainder>=(200*k)){
+            num200k=Math.floor((remainder/(200*k)));
+            remainder=remainder - num200k*200*k;
+        } if (remainder>=(100*k)){
+            num100k=Math.floor((remainder/(100*k)));
+            remainder=remainder - num100k*100*k;
+        } if (remainder>=(50*k)){
+            num50k=Math.floor((remainder/(50*k)));
+            remainder=remainder - num50k*50*k;
+        } if (remainder>=(20*k)){
+            num20k=Math.floor((remainder/(20*k)));
+            remainder=remainder - num20k*20*k;
+        } if (remainder>=(10*k)){
+            num10k=Math.floor((remainder/(10*k)));
+            remainder=remainder - num10k*10*k;
+        } if (remainder>=(5*k)){
+            num5k=Math.floor((remainder/(5*k)));
+            remainder=remainder - num5k*5*k;
+        } if (remainder>=(2*k)){
+            num2k=Math.floor((remainder/(2*k)));
+            remainder=remainder - num2k*2*k;
+        } if (remainder>=(1*k)){
+            num1k=Math.floor((remainder/(1*k)));
+        }
 }
 
 function showResult(){
